03ea66f9ddb142f31289f78cb620d4b2be830b24
Alov Maxim <alovmax@yandex.ru>
2015-05-05 12:27:13 +0400

    [ADD] Zlib static sources

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,6 +3,14 @@ SOURCE_PATH := ./src
 INCLUDE_PATH := ./include
 PNG_SOURCE_PATH := ./png
 PNG_INCLUDE_PATH := ./png/include
+ZLIB_SOURCE_PATH := ./zlib
+
+include $(CLEAR_VARS)
+LOCAL_MODULE     := zlib
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(ZLIB_SOURCE_PATH)
+LS_C             := $(subst $(LOCAL_PATH)/$(ZLIB_SOURCE_PATH),$(ZLIB_SOURCE_PATH),$(wildcard $(LOCAL_PATH)/$(ZLIB_SOURCE_PATH)/*.c))
+LOCAL_SRC_FILES  := $(call LS_C,$(LOCAL_PATH))
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := png16
@@ -15,5 +23,5 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(INCLUDE_PATH) $(LOCAL_PATH)/$(PNG_INCLUDE_PA
 LS_CPP           := $(subst $(LOCAL_PATH)/$(SOURCE_PATH),$(SOURCE_PATH),$(wildcard $(LOCAL_PATH)/$(SOURCE_PATH)/*.cpp))
 LOCAL_SRC_FILES  := $(call LS_CPP,$(LOCAL_PATH))
 LOCAL_LDLIBS     := -llog -ldl -lz -landroid -lEGL -lGLESv2
-LOCAL_STATIC_LIBRARIES += png16
+LOCAL_STATIC_LIBRARIES += png16 zlib
 include $(BUILD_SHARED_LIBRARY)
