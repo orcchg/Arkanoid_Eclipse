2cc518fcaf9a481349541b3b7599e51ed31bfd26
Alov Maxim <alovmax@yandex.ru>
2015-05-08 11:58:37 +0400

    [REWIND] Back levels

--- src/com/orcchg/arkanoid/surface/Levels.java
+++ src/com/orcchg/arkanoid/surface/Levels.java
@@ -16,34 +16,25 @@ public class Levels {
     }
   }
   
-//  private static final String[] L0 = new String[] {"          ",
-//                                                   "          ",
-//                                                   "          ",
-//                                                   "          ",
-//                                                   "  SSSSSS  ",
-//                                                   "  SSSSSS  ",
-//                                                   "  SSSSSS  ",
-//                                                   "  SSSSSS  "};
-//  
-//  private static final String[] L1 = new String[]{"          ",
-//                                                  "          ",
-//                                                  "          ",
-//                                                  "          ",
-//                                                  "  BBBBBB  ",
-//                                                  "  BSSSSB  ",
-//                                                  "  BSSSSB  ",
-//                                                  "  BSSSSB  ",
-//                                                  "  BSSSSB  ",
-//                                                  "  BBBBBB  "};
-  
   private static final String[] L0 = new String[] {"          ",
-    "          ",
-    "          ",
-    "   S      "};
-
-private static final String[] L1 = new String[]{"          ",
-   "       S  ",
-   "          "};
+                                                   "          ",
+                                                   "          ",
+                                                   "          ",
+                                                   "  SSSSSS  ",
+                                                   "  SSSSSS  ",
+                                                   "  SSSSSS  ",
+                                                   "  SSSSSS  "};
+  
+  private static final String[] L1 = new String[]{"          ",
+                                                  "          ",
+                                                  "          ",
+                                                  "          ",
+                                                  "  BBBBBB  ",
+                                                  "  BSSSSB  ",
+                                                  "  BSSSSB  ",
+                                                  "  BSSSSB  ",
+                                                  "  BSSSSB  ",
+                                                  "  BBBBBB  "};
   
   private static final String[] L2 = new String[]{"          ",
                                                   "          ",
